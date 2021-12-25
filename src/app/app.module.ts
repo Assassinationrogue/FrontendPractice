1aacd3fb7a98a1e20f3db3ad7e5721cece4f9f32
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2021-12-25 22:46:16 +0530

    SB-2 # components initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddComponent } from './components/add/add.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    FormComponent,
+    ListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
