44d3a111977e3f5fa6c71af674528629b7ba91b5
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2021-12-30 10:20:54 +0530

    SB-10 # customised popup implementation in angular

--- src/app/components/task-action/view-edit-task-popup/view-edit-task-popup.component.ts
+++ src/app/components/task-action/view-edit-task-popup/view-edit-task-popup.component.ts
@@ -4,14 +4,20 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'view-edit-task-popup',
   templateUrl: './view-edit-task-popup.component.html',
-  styleUrls: ['./view-edit-task-popup.component.scss']
+  styleUrls: ['./view-edit-task-popup.component.scss'],
 })
 export class ViewEditTaskPopupComponent implements OnInit {
-  @Input() hasCalled: boolean = false;
-  @Input() task: addTask;
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set hasCalled(value: boolean) {
+    if (value) {
+      this.togglePopup();
+    }
   }
+  @Input() task: addTask;
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  togglePopup(): void {
+    document.getElementById('popup-1').classList.toggle('active');
+  }
 }
