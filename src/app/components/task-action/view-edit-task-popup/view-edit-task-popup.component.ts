a9a100486d732d31aee317e9fb65994a9dca48ce
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-01-01 17:27:20 +0530

    SB-10 # fix for popup not working for multiple task

--- src/app/components/task-action/view-edit-task-popup/view-edit-task-popup.component.ts
+++ src/app/components/task-action/view-edit-task-popup/view-edit-task-popup.component.ts
@@ -1,7 +1,7 @@
 import { addTask } from 'src/app/model/task';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'view-edit-task-popup',
@@ -13,7 +13,18 @@ export class ViewEditTaskPopupComponent implements OnInit {
   @Output() editedValue = new EventEmitter<addTask>();
   @Input() set hasCalled(value: Observable<boolean>) {
     if (value) {
-      value.pipe(take(1)).subscribe((state) => console.log(state));
+      value
+        .pipe(
+          take(1),
+          map((data) => {
+            if (data) {
+              return true;
+            } else {
+              return false;
+            }
+          })
+        )
+        .subscribe((state) => this.togglePopup(state));
     }
   }
   @Input() task: addTask;
@@ -30,9 +41,11 @@ export class ViewEditTaskPopupComponent implements OnInit {
    * @returns void
    */
   togglePopup(state: boolean): void {
-    document.getElementById('popup-1').classList.toggle('active');
     if (!state) {
       this.closed.emit(state);
+      document.getElementById('popup-1').classList.remove('active');
+    } else {
+      document.getElementById('popup-1').classList.add('active');
     }
   }
 
